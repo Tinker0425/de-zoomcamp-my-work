915db4d54a90dc183800ed3daa3d3da9fa3b8a7d
Flynn25 <Kayla425@users.noreply.github.com>
2025-01-19 11:01:12 -0900

    :building_construction: module-01 video2

---
+++ module-01/docker/video_2/README.md
@@ -0,0 +1,6 @@
+## Video 2 - 1.2.2  Ingesting NY Taxi Data to Postgres
+
+- My 1.2.2 gitbook notes [here](https://data-engineering-zoomcamp-2025-t.gitbook.io/tinker0425/module-1/running-postgres-locally-with-docker/1.2.2-ingesting-ny-taxi-data-to-postgres)
+
+
+
---
+++ module-01/docker/video_2/postgres_connect.sh
@@ -0,0 +1,12 @@
+# copy paste to base terminal window
+docker run -it \
+  -e POSTGRES_USER="root" \
+  -e POSTGRES_PASSWORD="root" \
+  -e POSTGRES_DB="ny_taxi" \
+  -v $(pwd)/ny_taxi_postgres_data:/var/lib/postgresql/data \
+  -p 5432:5432 \
+  postgres:13
+
+# pgcli from base
+pip install pgcli
+pgcli -h localhost -p 5432 -u root -d ny_taxi
